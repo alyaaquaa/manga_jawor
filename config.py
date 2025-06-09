17e001ba8814d9d645291710b38f0d5a69fdd0ec
alyaaquaa <162503282+alyaaquaa@users.noreply.github.com>
2025-06-09 16:51:22 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 from dotenv import load_dotenv
 import os
-load_dotenv() 
 
-class Config: SECRET_KEY = os.getenv('SECRET_KEY', 'defaultsecretkey') 
+load_dotenv()
 
-# Używamy pełnego URI do PostgreSQL z Rendera (ustawionego jako zmienna środowiskowa) 
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
-SQLALCHEMY_TRACK_MODIFICATIONS = False 
- 
-ADMIN_USERNAME = os.getenv('ADMIN_USERNAME') 
-ADMIN_EMAIL = os.getenv('ADMIN_EMAIL') 
-ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'defaultsecretkey')
+
+    # Muszą być wewnątrz klasy!
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    ADMIN_USERNAME = os.getenv('ADMIN_USERNAME')
+    ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
+    ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
