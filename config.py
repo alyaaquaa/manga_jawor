1b2887de87a84f35adc4f51682b37d7a517cfa2b
alyaaquaa <162503282+alyaaquaa@users.noreply.github.com>
2025-06-09 16:36:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,11 @@
-from dotenv import load_dotenv
-import os
+load_dotenv() 
 
-# Załaduj zmienne środowiskowe z pliku .env
-load_dotenv()
+class Config: SECRET_KEY = os.getenv('SECRET_KEY', 'defaultsecretkey') 
 
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'defaultsecretkey')
-
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_NAME = os.getenv('DB_NAME')
-
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    ADMIN_USERNAME = os.getenv('ADMIN_USERNAME')
-    ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
-    ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
-
+# Używamy pełnego URI do PostgreSQL z Rendera (ustawionego jako zmienna środowiskowa) 
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
+SQLALCHEMY_TRACK_MODIFICATIONS = False 
+ 
+ADMIN_USERNAME = os.getenv('ADMIN_USERNAME') 
+ADMIN_EMAIL = os.getenv('ADMIN_EMAIL') 
+ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
